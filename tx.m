569e6fdd8aa06df63ccf34b4eaed5574c4ace8a3
Ahmedounet <56503724+Ahmedounet@users.noreply.github.com>
2020-11-10 10:49:27 +0100

    Update tx.m
    Tx in gray mapping

--- tx.m
+++ tx.m
@@ -67,8 +67,8 @@ QPSK=2;
    
     elseif(mapping_type==QPSK)
         
-        NonGrayMap = 1/sqrt(2) * [( 1-1j) ( 1+1j) (-1+1j) (-1-1j)];
-        out_signal = NonGrayMap(bi2de(input_signal)+1).' ;
+        GrayMap = 1/sqrt(2) * [(-1-1j) (-1+1j) ( 1-1j) ( 1+1j)];
+        out_signal = GrayMap(bi2de(input_signal)+1).' ;
         
     else
         disp('Incorrect mapping type')
@@ -94,26 +94,4 @@ function passband_signal=up_conversion(baseband_signal, fc)
 
 end
 
-function h = rrc(os_factor, rolloff_factor, filterlength)
-% Returns a the FIR coefficients of a Root Raised Cosine filter.
-% os_factor is the oversampling factor, typically set to 4.
-% rolloff_factor is a in [0; 1]. UMTS for instance uses a rolloff factor of 0.22.
-% filterlength is the _onesided_ filterlength, i.e. the total number of taps is 2*filterlength+1.
-%
-% Note that the current implementation of this function does not handle the case when the denominator becomes zero,
-% which means that the rolloff_factor must be chosen so that os_factor/(4*rolloff_factor) is not be an integer.
-% In this case, choose a slightly different rolloff factor, or fix this implementation...
-
-n = (-filterlength : filterlength)' / os_factor;
-
-if rolloff_factor == 0, % special case, just return a sinc
-	h = sinc(n);
-else
-	if floor(os_factor/(4*rolloff_factor)) == os_factor/(4*rolloff_factor), error('os_factor/(4*rolloff_factor) must not be an integer.'); end
-	h = (4*rolloff_factor/pi * cos((1+rolloff_factor)*pi*n) + (1-rolloff_factor)*sinc((1-rolloff_factor)*n)) ./ (1 - (4*rolloff_factor*n).^2);
-end
-
-% Normalize to a total power of 1.
-h = h / sqrt(sum(abs(h).^2));
-end
 
