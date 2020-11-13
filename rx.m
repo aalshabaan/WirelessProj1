d739e15606e9dc7085ca54ae711ab1f57ae404fd
AbedShabaan <abed.shabaan@epfl.ch>
2020-11-13 11:42:31 +0100

    rx working, but abysmal BER

--- rx.m
+++ rx.m
@@ -1,4 +1,4 @@
-function [rxbits, conf] = rx(rxsignal,conf,k)
+function [rxbits, conf, raw_bits] = rx(rxsignal,conf,k)
 % Digital Receiver
 %
 %   [txsignal conf] = tx(txbits,conf,k) implements a complete causal
@@ -17,25 +17,28 @@ function [rxbits, conf] = rx(rxsignal,conf,k)
 % dummy 
 
 % Downconversion
-down_converted = rxsignal*exp(conf.f_c*2*pi*1i);
+down_converted = rxsignal*exp(-conf.f_c*2*pi*1i);
 
 %Lowpass filtering:
 
 down_converted = lowpass(down_converted, conf);
 % Matched-filter
 rolloff = 0.22;
-pulse = rrc(conf.os_factor, rolloff, 20);
+pulse = rrc(conf.os_factor, rolloff, conf.mf_length);
 filtered_rx = conv(pulse,down_converted);
 
 % Frame sync
 preamble = preamble_generate(conf.npreamble);
 % Map preamble to BPSK
-preamble = 1 - 2*preamble;
+preamble = 2*preamble - 1;
 [data_idx, peak_phase] = frame_sync(filtered_rx,preamble,conf.os_factor);
+disp(['FrameSyncIdx ', num2str(data_idx)])
 
 % Time and phase estimation and Interpolation
 
-data_length = length(filtered(data_idx:end));
+data_length = floor(length(filtered_rx(data_idx:end))/conf.os_factor);
+disp(['nb_frames: ', num2str(data_length)])
+% data_length = conf.nbits;
 cum_err = 0;
 diff_err = zeros(1,data_length);
 epsilon  = zeros(1,data_length);
@@ -46,30 +49,32 @@ theta_hat(1) = peak_phase;
 
 for i=1:data_length
     
-     idx_start  = data_idx+(i-1)*os_factor;
+     idx_start  = data_idx+(i-1)*conf.os_factor;
      
-     idx_range  = idx_start:idx_start+os_factor-1;
-     segment    = filtered_rx_signal(idx_range);
+     idx_range  = idx_start:idx_start+conf.os_factor-1;
+     segment    = filtered_rx(idx_range);
     
      % Time error estimatuon
      pwr         = abs(segment).^2;
-     diff_err(i) = [1 -1j -1 1j]*pwr; % Calculate the power spectrum
+     fourier_exponents = exp(2*pi*1i*(1:conf.os_factor)/conf.os_factor);
+     diff_err(i) = fourier_exponents*pwr; % Calculate the power spectrum
      cum_err     = cum_err + diff_err(i);
      epsilon(i)  = -1/(2*pi)*angle(cum_err);
      
-     sample_diff   = floor(epsilon(ii)*os_factor); % integer
-     int_diff      = mod(epsilon(ii)*os_factor,1); % interval [0 1)
+     sample_diff   = floor(epsilon(i)*conf.os_factor); % integer
+     int_diff      = mod(epsilon(i)*conf.os_factor,1); % interval [0 1)
     
-     % Phase Estimation
-     deltaTheta = 1/4*angle(-payload_data(i)^4) + pi/2*(-1:4);
-     [~, ind] = min(abs(deltaTheta - theta_hat(i)));
-     theta = deltaTheta(ind);
-     theta_hat(i+1) = mod(0.01*theta + 0.99*theta_hat(i), 2*pi);
-     
      % linear interpolateion
      y     = filtered_rx(idx_start+sample_diff:idx_start+sample_diff+1);
      y_hat = y(1)+int_diff*(y(2)-y(1));
      data(i) = y_hat;
+     
+     % Phase Estimation
+     deltaTheta = 1/4*angle(-data(i)^4) + pi/2*(-1:4);
+     [~, ind] = min(abs(deltaTheta - theta_hat(i)));
+     theta = deltaTheta(ind);
+     theta_hat(i+1) = mod(0.01*theta + 0.99*theta_hat(i), 2*pi);
+     
      
      data(i) = data(i) * exp(-1i * theta_hat(i+1));
      
@@ -77,23 +82,27 @@ for i=1:data_length
 end
 
 
-% Downsample
-downsampled_data = data(1:conf.os_factor:end);
-L = length(downsampled_data);
-
 % Demapping
-BPSK_map = [1 -1];
+BPSK_map = [-1 1];
 QPSK_map =  1/sqrt(2) * [(-1-1j) (-1+1j) ( 1-1j) ( 1+1j)];
 
-switch conf.os_factor
+switch conf.modulation_order
     case 1 %BPSK
-        [~,ind] = min((ones(L,2)*diag(BPSK_map) - diag(downsampled_data)*ones(L,2)),[],2);
-        rx_bits = de2bi(ind-1);
+        disp('BPSK')
+        [~,ind] = min((ones(data_length,2)*diag(BPSK_map) - diag(data)*ones(data_length,2)),[],2);
+        rxbits = de2bi(ind-1);
+        % Unfold into a single column stream
+        raw_bits = rxbits;
+        rxbits = rxbits(1:conf.nbits);
         
     case 2 %QPSK
-        [~,ind] = min((ones(L,4)*diag(QPSK_map) - diag(downsampled_data)*ones(L,4)),[],2);
-        rx_bits = de2bi(ind-1);
+        disp('QPSK')
+        [~,ind] = min((ones(data_length,4)*diag(QPSK_map) - diag(data)*ones(data_length,4)),[],2);
+        rxbits = de2bi(ind-1);
+        % Unfold into a single column stream
+        raw_bits = rxbits;
+        rxbits = rxbits(1:conf.nbits)';
     otherwise
-        disp('WTF?');
-        rx_bits = zeros(conf.nbits,1);
+        disp('WTF?')
+        rxbits = zeros(conf.nbits,1);
 end
