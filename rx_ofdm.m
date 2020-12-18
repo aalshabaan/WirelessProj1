25ecc924a127c0173dc053a2b4acd151d18059e5
Ahmedounet <56503724+Ahmedounet@users.noreply.github.com>
2020-12-18 03:26:11 +0100

    updates on rx

--- rx_ofdm.m
+++ rx_ofdm.m
@@ -1,9 +1,8 @@
 function [rx_signal,conf] = rx_ofdm(rx_bits,conf,k)
 
-
 %Low pass
-%f=
-%filtered_rx=ofdmlowpass(rx_bits,conf,f)
+%cut_of_freq=
+%filtered_rx=ofdmlowpass(rx_bits,conf,cut_of_freq)
 
 
 % Frame sync
@@ -16,7 +15,9 @@ disp(['FrameSyncIdx ', num2str(data_idx)])
 
 %remove cp
 
-conf.os_factor_ofdm
+
+%cp_length=floor(conf.ncp*conf.os_factor_ofdm)
+%remove_cp(filtered_rx, conf.os_factor_ofdm, cp_length)
 
 
 
@@ -29,8 +30,43 @@ conf.os_factor_ofdm
 
 %Back to frequency domain
 
-freq_signal=osfft(,conf.os_factor_ofdm)
+%freq_signal=osfft(,conf.os_factor_ofdm)
+
+%Equalizer
+
+%a=inv(osfft(h))*freq_signal
 
 
+%demapping
 
 end
+
+
+
+function output=remove_cp(input, len_symbol, cp_length)
+%remove_cp: removes cyclic prefix from recieved signal
+%input: input vector to remove the cp from
+%len_symbol: length of a symbol (without cp)
+%cp_length: length of cp
+
+%output: cleaned input vector from any cp
+
+    num_symbols=floor(size(input,1)/(cp_length+len_symbol));
+    output=[];
+    
+        for i=1:num_symbols
+            output(:,i)=input((i-1)*(len_symbol+cp_length)+cp_length+1:(i-1)*(len_symbol+cp_length)+cp_length+len_symbol);
+
+        end
+        
+    output=reshape(output,[],1);
+end
+
+
+
+
+
+
+
+
+
